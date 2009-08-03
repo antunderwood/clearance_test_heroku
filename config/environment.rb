83e81410e2a8b7359c8b8ba712849c297a32e937
Anthony Underwood <email2ants@gmail.com>
2009-08-03 19:33:27 +0100

    add tls email functionality

--- config/environment.rb
+++ config/environment.rb
@@ -38,4 +38,18 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+config.action_mailer.delivery_method = :smtp 
+config.action_mailer.smtp_settings = { 
+:enable_starttls_auto => :true, 
+:address => "smtp.gmail.com", 
+:port => 587, 
+:domain => "gmail.com", 
+:authentication => :plain, 
+:user_name => "email2ants@gmail.com", 
+:password => "shoptrop4610", 
+:tls => :true 
+} 
+config.action_mailer.perform_deliveries = :true 
+config.action_mailer.raise_delivery_errors = :true 
+config.action_mailer.default_charset = "utf-8"
 end
